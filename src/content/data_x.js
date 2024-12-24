add28d5ceae053a4f2082562ab1df7aba1614820
Andres Leon <noelserdna@gmail.com>
2024-12-24 02:02:51 +0100

    roadmap y tokenomics

---
+++ src/content/data_x.js
@@ -0,0 +1,36 @@
+const dataX = [
+    {
+      id: 'https://x.com/RadixCTO/status/1870814116962865663',
+      data: {
+        title: 'Stay cozy, stay Radix, and let’s make today radically productive. 🚀',
+        img: 'https://pbs.twimg.com/media/GfZ3e6IaIAATkbh?format=jpg&name=large',
+        img_alt: 'Stay cozy, stay Radix, and let’s make today radically productive. 🚀'
+      }
+    },
+    {
+      id: 'https://x.com/RadixCTO/status/1870653641629282694',
+      data: {
+        title: 'Newbies panic at the bottom, veterans double down and play the long game. 🃏🐾',
+        img: 'https://pbs.twimg.com/media/GfXlkh3bQAAgJRA?format=jpg&name=large',
+        img_alt: 'Newbies panic at the bottom, veterans double down and play the long game. 🃏🐾'
+      }
+    },
+    {
+      id: 'https://x.com/RadixCTO/status/1870066045400297983',
+      data: {
+        title: '🐾♟️ RadixCTO doesn’t chase the noise; we strategize, adapt, and prove our strengths when it matters most.',
+        img: 'https://pbs.twimg.com/media/GfPPI0OasAEveb4?format=jpg&name=large',
+        img_alt: '🐾♟️ RadixCTO doesn’t chase the noise; we strategize, adapt, and prove our strengths when it matters most.'
+      }
+    },
+    {
+      id: 'https://x.com/RadixCTO/status/1869737208477032884',
+      data: {
+        title: 'It was good while it lasted… $XRD and $CTO, the adventure is over. Time to head back to the mothership. 🛸',
+        img: 'https://pbs.twimg.com/media/GfKkFL8WMAAsmCC?format=jpg&name=large',
+        img_alt: 'It was good while it lasted… $XRD and $CTO, the adventure is over. Time to head back to the mothership. 🛸'
+      }
+    }
+  ];
+
+  export default dataX;
